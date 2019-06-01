ff0f46fced64ec0c7045a689ab596951cae05116
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-06-01 14:19:42 +0700

    Fix counter position again lol

--- src/sections/header.js
+++ src/sections/header.js
@@ -56,6 +56,7 @@ const Nav = () => {
     <section
       css={css`
         position: fixed;
+        z-index: 1;
         top: 0;
         left: 0;
         right: 0;
@@ -251,7 +252,7 @@ const Header = () => (
         position: absolute;
         left: 0;
         right: 0;
-        bottom: 220px;
+        bottom: 140px;
       `}
     >
       <Countdown />
