3d17a54b097047fd34300e471a07657a5ab94690
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-06-03 13:54:17 +0700

    Add first section of event schedule

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Mobile Conference Thailand 2019`,
+    title: `Mobile Conf Thailand 2019`,
     description: `biggest mobile conference in Thailand`,
-    author: `@iboss-ptk`,
+    author: `@iboss-ptk`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,8 +11,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -25,8 +25,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
@@ -34,13 +34,13 @@ module.exports = {
         fonts: [
           {
             family: `Montserrat`,
-            variants: ["300", "400", "500", "600", "700"],
+            variants: ["300", "400", "500", "600", "700"]
           }
         ]
       }
-    },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
-}
+  ]
+};
--- src/sections/schedule.js
+++ src/sections/schedule.js
@@ -1,13 +1,129 @@
 import React from "react";
 import GenSection from "../components/gen-section";
 import { color } from "../components/common";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+
+const headerStyle = css`
+  background-color: ${color.green};
+  color: white;
+
+  font-weight: 600;
+  height: 52px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 24px;
+`;
+
+const Time = styled.div`
+  background-color: rgba(124, 124, 124, 0.1);
+  width: 125px;
+  color: ${color.darkGray};
+  padding: 10px;
+`;
+
+const Session = ({ title, speaker }) => (
+  <div
+    css={css`
+      display: flex;
+      padding: 10px 24px;
+      justify-content: flex-start;
+      text-align: left;
+      flex-direction: column;
+    `}
+  >
+    <div
+      css={css`
+        font-weight: 600;
+      `}
+    >
+      {title}
+    </div>
+    <div
+      css={css`
+        font-size: 12px;
+        color: ${color.darkGray};
+      `}
+    >
+      {speaker}
+    </div>
+  </div>
+);
 
 const Schedule = () => (
   <GenSection id="schedule" title="EVENT SCHEDULE">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat.
+    <div
+      css={css`
+        display: grid;
+        grid-template-columns: 125px auto 175px;
+        grid-template-rows: 52px auto 52px;
+      `}
+    >
+      <div
+        css={[
+          headerStyle,
+          css`
+            width: 125px;
+          `
+        ]}
+      >
+        Time
+      </div>
+      <div
+        css={[
+          headerStyle,
+          css`
+            justify-content: flex-start;
+          `
+        ]}
+      >
+        Sessions
+      </div>
+      <div css={headerStyle}>Venue</div>
+
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: 125px auto;
+          grid-column: span 2;
+        `}
+      >
+        <Time>8:30 am</Time>
+        <Session title="Registration" />
+
+        <Time>9:30 am</Time>
+        <Session title="Opening Remark" speaker="Organizer & Gosoft" />
+
+        <Time>10:30 am</Time>
+        <Session
+          title="Automating at scale:Challenges and (some) solutions"
+          speaker="Max Panasenkov, iOS developer at Adoda"
+        />
+      </div>
+
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        Main Hall
+      </div>
+
+      <div
+        css={css`
+          grid-column: 1 / span 3;
+          background-color: ${color.gray};
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        Lunch break for 80 mins
+      </div>
+    </div>
   </GenSection>
 );
 
