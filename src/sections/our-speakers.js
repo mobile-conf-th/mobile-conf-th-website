e82b812cd85783d470d0f7c78c7132c0bbd70774
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-06-03 11:51:07 +0700

    Add topic detail for speaker

--- src/sections/our-speakers.js
+++ src/sections/our-speakers.js
@@ -6,7 +6,7 @@ import { StaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
 import { css } from "@emotion/core";
 
-const Speaker = ({ img, name, title }) => {
+const Speaker = ({ img, name, title, topic, time, room }) => {
   const [isShowTopic, setShowTopic] = useState(false);
   return (
     <div
@@ -43,8 +43,61 @@ const Speaker = ({ img, name, title }) => {
                  opacity: 0.9;
                }`};
             background-color: ${color.blue};
+            color: #f1f2f2;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
           `}
-        />
+        >
+          <span
+            css={css`
+              font-size: 13.5px;
+              font-weight: normal;
+              font-style: italic;
+              padding: 5px;
+              display: block;
+            `}
+          >
+            - TOPIC -
+          </span>
+          <span
+            css={css`
+              font-size: 20px;
+              font-weight: normal;
+              display: block;
+              padding: 5px;
+            `}
+          >
+            {topic}
+          </span>
+          <div
+            css={css`
+              background-color: #f1f2f2;
+              width: 56px;
+              height: 3px;
+              margin: 10px;
+            `}
+          />
+          <span
+            css={css`
+              font-size: 16.5px;
+              display: block;
+              padding-top: 5px;
+            `}
+          >
+            {time}
+          </span>
+          <span
+            css={css`
+              font-size: 16.5px;
+              display: block;
+              padding-bottom: 5px;
+            `}
+          >
+            {room}
+          </span>
+        </div>
       </div>
       <span
         css={css`
@@ -90,31 +143,49 @@ const OurSpeakers = () => (
             img={data.speaker.childImageSharp.fixed}
             name="Pitiphong Phongpattranont"
             title="iOS developer, Agoda"
+            topic="Mobile first approach in prototype and layers of experience design"
+            time="2.00 pm - 3.00 pm"
+            room="Hall 1"
           />
           <Speaker
             img={data.speaker.childImageSharp.fixed}
             name="Pitiphong Phongpattranont"
             title="iOS developer, Agoda"
+            topic="Mobile first approach in prototype and layers of experience design"
+            time="2.00 pm - 3.00 pm"
+            room="Hall 1"
           />
           <Speaker
             img={data.speaker.childImageSharp.fixed}
             name="Pitiphong Phongpattranont"
             title="iOS developer, Agoda"
+            topic="Mobile first approach in prototype and layers of experience design"
+            time="2.00 pm - 3.00 pm"
+            room="Hall 1"
           />
           <Speaker
             img={data.speaker.childImageSharp.fixed}
             name="Pitiphong Phongpattranont"
             title="iOS developer, Agoda"
+            topic="Mobile first approach in prototype and layers of experience design"
+            time="2.00 pm - 3.00 pm"
+            room="Hall 1"
           />
           <Speaker
             img={data.speaker.childImageSharp.fixed}
             name="Pitiphong Phongpattranont"
             title="iOS developer, Agoda"
+            topic="Mobile first approach in prototype and layers of experience design"
+            time="2.00 pm - 3.00 pm"
+            room="Hall 1"
           />
           <Speaker
             img={data.speaker.childImageSharp.fixed}
             name="Pitiphong Phongpattranont"
             title="iOS developer, Agoda"
+            topic="Mobile first approach in prototype and layers of experience design"
+            time="2.00 pm - 3.00 pm"
+            room="Hall 1"
           />
         </div>
       </GenSection>
