21d85513218bcc4074e0197b42be8db422b0794c
off <offfffz@gmail.com>
2019-07-10 20:16:40 +0700

    add location at s31 hotel

--- src/sections/location.js
+++ src/sections/location.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { css } from "@emotion/core";
 import GenSection from "../components/gen-section";
+import {onMobile} from '../components/common'
 
 const Location = () => (
   <GenSection id="location" title="LOCATION">
@@ -16,45 +17,46 @@ const Location = () => (
           padding: 0.9375rem;
         `}
       >
-        {/* Charn Issara Tower 1 */}
-        TBA
+        S31 Hotel
       </h4>
-      {/*   <p> */}
-      {/*     942 Rama 4 road, Khwaeng Suriya Wong, Khet Bang Rak, Krung Thep Maha */}
-      {/*     Nakhon 10500 */}
-      {/*   </p> */}
-      {/* </div> */}
-      {/* <div */}
-      {/*   css={css` */}
-      {/*     position: relative; */}
-      {/*     overflow: hidden; */}
-      {/*     padding-top: 56.25%; */}
-      {/*  */}
-      {/*     ${onMobile} { */}
-      {/*       padding-top: 100%; */}
-      {/*       margin: 0 -1.5rem -6.25rem; */}
-      {/*     } */}
-      {/*  */}
-      {/*     iframe { */}
-      {/*       position: absolute; */}
-      {/*       top: 0; */}
-      {/*       left: 0; */}
-      {/*       width: 100%; */}
-      {/*       height: 100%; */}
-      {/*       border: 0; */}
-      {/*     } */}
-      {/*   `} */}
-      {/* > */}
-      {/*   <iframe */}
-      {/*     title="map" */}
-      {/*     width="800" */}
-      {/*     height="300" */}
-      {/*     src="https://maps.google.com/maps?q=Charn%20Issara%20Tower%201&t=&z=13&ie=UTF8&iwloc=&output=embed" */}
-      {/*     frameBorder="0" */}
-      {/*     scrolling="no" */}
-      {/*     marginHeight="0" */}
-      {/*     marginWidth="0" */}
-      {/*   /> */}
+      <p>
+        545 Sukhumvit Rd, Khwaeng Khlong Toei Nuea, Khet Watthana,
+      </p>
+      <p>
+        Krung Thep Maha Nakhon 10110
+      </p>
+    </div>
+    <div
+      css={css`
+        position: relative;
+        overflow: hidden;
+        padding-top: 56.25%;
+
+        ${onMobile} {
+          padding-top: 100%;
+          margin: 0 -1.5rem -6.25rem;
+        }
+
+        iframe {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          border: 0;
+        }
+      `}
+    >
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.7552138776427!2d100.56382361550179!3d13.733264601421242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29ee9b762895f%3A0xcbffbfb218432fa1!2sS31+Sukhumvit+Hotel!5e0!3m2!1sen!2sth!4v1562764295297!5m2!1sen!2sth"
+        title="map"
+        width="800"
+        height="300"
+        frameBorder="0"
+        scrolling="no"
+        marginHeight="0"
+        marginWidth="0"
+      />
     </div>
   </GenSection>
 );
