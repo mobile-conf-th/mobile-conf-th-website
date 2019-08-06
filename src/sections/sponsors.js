2fdb70cdb6e9d4d631feba3faca298774efa9293
macbaszii <macbaszii@gmail.com>
2019-08-06 15:09:35 +0700

    updated wework logo in sponsor section

--- src/sections/sponsors.js
+++ src/sections/sponsors.js
@@ -31,7 +31,7 @@ const OurSponsors = styled(() => (
         }
         wework: file(relativePath: { eq: "sp-wework.png" }) {
           childImageSharp {
-            fixed(width: 80, height: 80) {
+            fixed(width: 357, height: 80) {
               ...GatsbyImageSharpFixed_tracedSVG
             }
           }
